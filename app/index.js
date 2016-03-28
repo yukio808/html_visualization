f70e4e8881c12919ce34f97e5f7342b5778f43e5
yukio yamamoto <aj.yamamoto@me.com>
2016-03-27 15:36:10 -1000

    parser showing attributes and tags Using es6 syntax next task is to parse the html into a readable javascript object

--- app/index.js
+++ app/index.js
@@ -1,18 +1,34 @@
+'use strict';
 const fs = require('fs');
-const parser = require('htmlparser2');
+const htmlParser = require('htmlparser2');
 const dir = __dirname;
 const testPath = '/../tests/test.html';
 const pathToFile = dir + testPath;
-const testHtmlFile = fs.readFile(pathToFile, 'utf8', logger);
+let html;
+const parser = new htmlParser.Parser({
+  onopentag: (name, attr) => {
+    console.log('onopentag', name, attr);
+  },
+  onattribute: (name, attr) => {
+    // console.log('atrributes of tags', name, attr);
+  },
+  onclosetag: (name) => {
+    console.log('closeTag', name);
+  }
+});
 
-// console.log('parser',parser);
+fs.readFile(pathToFile, 'utf8', callBack);
 
 // callback for fs and start of app
-function logger(err, data) {
+function callBack(err, data) {
   if (data !== undefined) {
-    console.log('data',data);
-    return data;
+    //console.log('data',data);
+    html = data;
   } else {
-    console.error(err);
+    const errArr = ["ERROR"];
+    errArr.push(err);
+    html = errArr;
   }
+  parser.write(html);
+  parser.end();
 }
